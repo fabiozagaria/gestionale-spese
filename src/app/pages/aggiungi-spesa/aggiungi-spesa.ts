b682587cccd098918ed3d085b2a5a0bd70d8d1c1
JavaMetalCoder <MetalCoder.dev@proton.me>
2026-05-07 21:19:50 +0200

    feat: implement reactive form for adding expenses with validation and update service methods

--- src/app/pages/aggiungi-spesa/aggiungi-spesa.ts
+++ src/app/pages/aggiungi-spesa/aggiungi-spesa.ts
@@ -1,9 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Expense } from '../../type/spesa';
+import { SpesaService } from '../../services/spesa-service';
 
 @Component({
   selector: 'app-aggiungi-spesa',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './aggiungi-spesa.html',
   styleUrl: './aggiungi-spesa.css',
 })
-export class AggiungiSpesa {}
+export class AggiungiSpesa {
+ private spesaService = inject(SpesaService);
+
+ protected formSpesa = new FormGroup({
+      importo: new FormControl<number | null>(null, [Validators.required, Validators.min(0.01)],
+      ),
+      categoria: new FormControl<string>('',
+        [Validators.required]
+      ),
+      descrizione: new FormControl<string>(''),
+      data: new FormControl<string>('', Validators.required)
+    });
+
+    onSubmit(): void {
+      if(this.formSpesa.invalid) {
+        this.formSpesa.markAsTouched();
+        return;
+      }
+
+      const valoreForm = this.formSpesa.getRawValue();
+
+      if (valoreForm.importo === null || !valoreForm.categoria || !valoreForm.data) {
+        return;
+      }
+
+      const nuovaSpesa: Expense = {
+        id: crypto.randomUUID(),
+        importo: valoreForm.importo,
+        categoria: valoreForm.categoria,
+        descrizione: valoreForm.descrizione ?? '',
+        data: valoreForm.data
+      };
+
+      this.spesaService.addExpense(nuovaSpesa);
+
+      this.formSpesa.reset({
+        importo: null,
+        categoria: '',
+        descrizione: '',
+        data: ''
+      })
+    }
+
+
+}
--- src/app/services/spesa-service.ts
+++ src/app/services/spesa-service.ts
@@ -8,12 +8,20 @@ export class SpesaService {
   private _allExpense: WritableSignal<Expense[]> = signal([]);
   readonly allExpense = this._allExpense.asReadonly();
 
-  public setExpenses(expenseArr: Expense[]): void {
+  private setExpenses(expenseArr: Expense[]): void {
     this._allExpense.set(expenseArr);
 
   }
 
-  public addExpence(expense: Expense): void {
+  public addExpense(expense: Expense): void {
     this._allExpense.update(prev => [...prev, expense])
   }
+
+  public removeExpense(id: string): void {
+    this._allExpense.update(prev => prev.filter(s => s.id === id))
+  }
+
+  
+
+ 
 }
